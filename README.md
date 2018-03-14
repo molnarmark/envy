28e7cdd5581045380e924184130d99a4bffc8c9c
Mark Molnar <molnarpw@gmail.com>
2018-03-14 19:51:29 +0100

    Added donation to README.md

--- README.md
+++ README.md
@@ -20,3 +20,8 @@ local EnvyEmitter = loadstring(Envy:new())()
 
 ### License
 MIT
+
+### Fancy buying me a beer?
+If this project was helpful to you, you can buy me a beer if you feel like doing so. 🙂
+
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=YM7E34E2LT4D8)
--- envy.lua
+++ envy.lua
@@ -1,10 +1,38 @@
 Envy = {}
 Envy.__index = Envy
 
-function Envy:new()
+function Envy:new(name)
   return [[
-  return { events = {}, on = function(self, name, callback) self.events[name] = callback end,
-  emit = function(self, name, payload) if self.events[name] then self.events[name](payload) return true end return false end,
-  once = function(self, name, payload) if self.events[name] then self.events[name](payload) self.events[name] = nil return true end return false end }
+    return {
+    	events = {},
+
+  		__register = function(self, name)
+  			self.events[name] = "envy" .. tostring(getTickCount()) .. name
+    	end,
+
+    	on = function(self, name, callback)
+    		self.__register(self, name)
+  	  	addEvent(self.events[name], true)
+  	  	addEventHandler(self.events[name], root, callback)
+    	end,
+
+    	emit = function(self, name, payload)
+    		if self.events[name] then
+  				triggerEvent(self.events[name], root, payload)
+    			return true
+    		end
+    		return false
+    	end,
+
+    	once = function(self, name, payload)
+    		if self.events[name] then
+    			triggerEvent(self.events[name], root, payload)
+    			self.events[name] = nil
+    			return true
+    		end
+
+    		return false
+    	end
+    }
   ]]
 end
